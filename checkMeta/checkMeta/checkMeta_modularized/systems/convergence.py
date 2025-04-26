1b33db4012b77e197bc7e53049b7ab8abfe85472
uatu-song <94.jsong@nhaschools.com>
2025-04-26 08:42:15 -0400

    Delete checkMeta directory

--- checkMeta/checkMeta/checkMeta_modularized/systems/convergence.py
+++
@@ -1,5 +0,0 @@
-# systems/convergence.py
-
-def detect_convergences(units_moves: dict) -> list:
-    convergences = []
-    return convergences
--- checkMeta/data/divisions/setup_data.py
+++
@@ -1,8 +0,0 @@
-from data.loaders import ensure_data_directories, update_data_directories, get_data_filepath
-
-def setup_data_directories():
-    """Create the data directory structure"""
-    print("Setting up data directories...")
-    ensure_data_directories()
-    update_data_directories()
-    print("Data directories created.")
