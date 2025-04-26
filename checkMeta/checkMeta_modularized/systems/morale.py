33a4c68cff798180ef2eac3b5c53b5c433bcff9c
uatu-song <94.jsong@nhaschools.com>
2025-04-26 08:56:44 -0400

    Delete checkMeta directory

--- checkMeta/checkMeta_modularized/systems/morale.py
+++
@@ -1,7 +0,0 @@
-# systems/morale.py
-
-def adjust_morale(unit: dict, won_match: bool) -> None:
-    morale = unit.get("morale", 50)
-    morale += 10 if won_match else -5
-    morale = max(0, min(100, morale))
-    unit["morale"] = morale
