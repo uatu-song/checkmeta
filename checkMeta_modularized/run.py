d454ff6ea9dc24e87cc8f43f465c30c21a626fbd
uatu-song <94.jsong@nhaschools.com>
2025-04-27 10:41:16 -0400

    Delete checkMeta_modularized directory

--- checkMeta_modularized/run.py
+++
@@ -1,7 +0,0 @@
-# run.py
-from simulator.meta_league_simulator import MetaLeagueSimulator
-
-if __name__ == "__main__":
-    simulator = MetaLeagueSimulator(stockfish_path=None)
-    lineups_file = "path_to_your_lineups.xlsx"
-    simulator.run_matchday(lineups_file)
